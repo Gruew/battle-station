1f0e04d6724ea8e901409bdf63315b72ae7acba4
Andre Green <acgreen1226@gmail.com>
2015-09-09 16:55:14 -0700

    adding cucumber

---
+++ features/support/world.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var zombie = require('zombie');
+
+function World(callback) {
+    this.browser = new zombie();
+    this.visit = function (url, callback) {
+        this.browser.visit(url, callback);
+    };
+    callback();
+}
+
+module.exports = World;
